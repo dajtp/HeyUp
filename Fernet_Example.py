505aff412660113103786443658c89a432814e1f
dajtp <105429229+dajtp@users.noreply.github.com>
2023-08-06 07:39:06 +0100

    Added Write to File for Encrypted Message

--- Fernet_Example.py
+++ Fernet_Example.py
@@ -1,16 +1,21 @@
 from cryptography.fernet import Fernet
+import os
 
 Key = Fernet.generate_key()
-print(Key)
+#print(Key)
 
 Key1 = Fernet(Key)
-print(Key1)
+#print(Key1)
 
 Input_Msg = input("What's The Message: ")
 Input_Msg_Enc = Input_Msg.encode("utf-8")
 
 Token_Message = Key1.encrypt(Input_Msg_Enc)
 print(Token_Message)
+
+enc_token_file = open("Encrypted_Msg.txt", "w")
+enc_token_file.write(str(Token_Message))
+enc_token_file.close
 
 Dec_Token_Msg = Key1.decrypt(Token_Message)
 print(Dec_Token_Msg)
