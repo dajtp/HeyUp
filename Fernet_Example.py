298d24224e114ef4d9120c8daef0d95ad474f2da
Jake Parry <jakeparry@email.com>
2023-08-07 10:52:59 +0100

    Increased Function

--- Fernet_Example.py
+++ Fernet_Example.py
@@ -1,7 +1,7 @@
 from cryptography.fernet import Fernet
 import os
 import socket
-
+import shutil
 
 
 Key = Fernet.generate_key()
@@ -13,6 +13,9 @@ Key1 = Fernet(Key)
 with open("TheSecretKey.txt", "w") as f:
     f.write(str(Key))
     f.close
+    
+Key_Location = input("Where would you like to safely store your private key?: ")
+shutil.move('TheSecretKey.txt', Key_Location)
 
 Input_Msg = input("What's The Message: ")
 Input_Msg_Enc = Input_Msg.encode("utf-8")
@@ -20,7 +23,8 @@ Input_Msg_Enc = Input_Msg.encode("utf-8")
 Token_Message = Key1.encrypt(Input_Msg_Enc)
 print(Token_Message)
 
-enc_token_file = open("Encrypted_Msg.txt", "w")
+Message_Location = input("Where would you like to store your encrypted message?: ")
+enc_token_file = open(str(Message_Location) + "/" + "Encrypted_Msg.txt", "w")
 enc_token_file.write(str(Token_Message))
 enc_token_file.close
 
