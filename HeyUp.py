d35e1d0ce5ce2771128239d608b1ddde2bbad133
Jake <105429229+dajtp@users.noreply.github.com>
2024-02-08 07:49:13 +0000

    Updates

--- HeyUp.py
+++ HeyUp.py
@@ -8,12 +8,13 @@ import cryptography
 import os
 import shutil
 import argparse
-import socket
 import sys
 import time
 import datetime
 import threading
 import configparser
+from tcppinglib import tcpping
+import socket
 
 # Importing from internal Python Scripts.
 
@@ -28,10 +29,44 @@ Server_Port = config['SERVER_CREDS']['Server_Port']
 HeyUp_User = config['SERVER_CREDS']['Server_User']
 HeyUp_Pass = config['SERVER_CREDS']['Server_Pass']
 
-print(Server_IP)
-print(Server_Port)
-print(HeyUp_User)
-print(HeyUp_Pass)
+# Ping Server
+def pingserver():
+    Host = tcpping(Server_IP, Server_Port, 2, 3, 0.1)
+    if Host.is_alive == True:
+        print("Server is up and running.")
+        pass
+    else:
+        print("Server is down. Exiting application.")
+        sys.exit()
 
+# Receive Messages
+def recvmsg():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    # Connect the socket to the port where the server is listening
+    server_address = (Server_IP, Server_Port)
+    print('connecting to {} port {}'.format(*server_address))
+    sock.connect(server_address)
+
+    try:
+        # Send data
+        message = 'This is the message.  It will be repeated.'
+        print('sending {!r}'.format(message))
+        sock.sendall(message.encode())
+
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            print('received {!r}'.format(data))
+
+    finally:
+        print('closing socket')
+        sock.close()
+
+pingserver()
 
